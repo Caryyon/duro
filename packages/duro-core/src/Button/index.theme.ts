2c878b18190d8ad81ce356cd04a01813961c5f23
Cary Wolff <boss@caryyon.com>
2024-02-10 09:10:38 -0600

    updates

--- packages/duro-core/src/Button/index.theme.ts
+++ packages/duro-core/src/Button/index.theme.ts
@@ -1,8 +1,13 @@
 export default {
   primary: {
     bg: 'primary',
+    borderRadius: 0,
+    border: 'none',
+    boxShadow: `4px 4px 0px 2px #E5E5E5`,
+    margin: 2,
   },
   secondary: {
     bg: 'secondary',
+    borderRadius: 0,
   },
 }
